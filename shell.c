445d826caad9721ed6fbd839fb2d0ebf606e3dbd
dmoore9131 <dominiquemoore776@gmail.com>
2023-08-09 12:02:54 -0700

    task0-16

--- shell.c
+++ shell.c
@@ -27,6 +27,15 @@ perror("fgets");
 exit(EXIT_FAILURE);
 }
 input[strcspn(input, "\n")] = '\0'; 
+char *args[MAX_INPUT_SIZE];
+int arg_count = 0;
+char *token = strtok(input, " ");
+while (token != NULL) 
+{
+args[arg_count++] = token;
+token = strtok(NULL, " ");
+}
+args[arg_count] = NULL; 
 pid_t child_pid = fork();
 if (child_pid == -1) 
 {
@@ -35,9 +44,9 @@ exit(EXIT_FAILURE);
 } 
 else if (child_pid == 0) 
 {
-if (execlp(input, input, NULL) == -1) 
+if (execvp(args[0], args) == -1) 
 {
-perror("execlp");
+perror("execvp");
 exit(EXIT_FAILURE);
 }
 } 
