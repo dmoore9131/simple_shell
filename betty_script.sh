1d5547e2e459842ad2ffbda660d159cafaee82ca
dmoore9131 <dominiquemoore776@gmail.com>
2023-08-22 19:57:42 -0700

    task0

--- betty_script.sh
+++ betty_script.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# Your shell implementation will go here
+
 print_message() {
     echo "Hello, Betty! This script follows your guidelines."
 }
+
+# Implement other functions as needed
 
 main() {
     print_message
