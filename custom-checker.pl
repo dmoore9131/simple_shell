78846749a702b902f6b6676c01a8bae0fefd9e03
dmoore9131 <dominique.moore@ymail.com>
2023-08-23 15:48:26 -0700

    task0

---
+++ custom-checker.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl -w
+
+# Betty Style Checker v1.0
+# By Julien Barbier
+
+# ...
+
+# Your Betty checker script should be placed here.
+# This script checks the coding style of your code files.
+# Make sure to follow the coding style guidelines provided in the project description.
+
