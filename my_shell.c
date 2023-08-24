18a48730b23f3684163aabd66552ad0d150e5da0
dmoore9131 <dominique.moore@ymail.com>
2023-08-23 18:26:47 -0700

    task1

--- my_shell.c
+++ my_shell.c
@@ -1,65 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
-#define MAX_INPUT_LENGTH 1024
+#define MAX_INPUT_SIZE 1024
 
-int main(void) {
-    char input[MAX_INPUT_LENGTH];
+void display_prompt() {
+    printf("#cisfun$ ");
+    fflush(stdout);
+}
+
+int main() {
+    char input[MAX_INPUT_SIZE];
+    int status;
 
     while (1) {
-        printf("#cisfun$ ");
-        fflush(stdout);
+        display_prompt();
 
-        if (fgets(input, sizeof(input), stdin) == NULL) {
+        if (fgets(input, MAX_INPUT_SIZE, stdin) == NULL) {
+            // Handle end of file (Ctrl+D)
             printf("\n");
-            break; // Ctrl+D (end of file) was pressed
+            break;
         }
 
-        // Remove newline character from input
+        // Remove the newline character
         input[strcspn(input, "\n")] = '\0';
 
-        // Fork a new process
         pid_t pid = fork();
 
-        if (pid == -1) {
+        if (pid < 0) {
             perror("Fork error");
-            exit(EXIT_FAILURE);
         } else if (pid == 0) {
             // Child process
-
-            // Tokenize the input into command and arguments
-            char *args[MAX_INPUT_LENGTH];
-            char *token = strtok(input, " ");
-            int i = 0;
-
-            while (token != NULL) {
-                args[i++] = token;
-                token = strtok(NULL, " ");
-            }
-            args[i] = NULL;
-
-            if (execvp(args[0], args) == -1) {
-                perror("Error");
-                exit(EXIT_FAILURE);
+            if (execlp(input, input, (char *)NULL) == -1) {
+                // Try with the full path to the command
+                if (execlp("/bin/ls", "/bin/ls", (char *)NULL) == -1) {
+                    perror("Command not found");
+                    exit(EXIT_FAILURE);
+                }
             }
         } else {
             // Parent process
-            int status;
             waitpid(pid, &status, 0);
-
-            if (WIFEXITED(status)) {
-                int exit_status = WEXITSTATUS(status);
-                if (exit_status != 0) {
-                    fprintf(stderr, "Command exited with status %d\n", exit_status);
-                }
-            } else if (WIFSIGNALED(status)) {
-                int signal_number = WTERMSIG(status);
-                fprintf(stderr, "Command terminated by signal %d\n", signal_number);
-            }
         }
     }
 
