fa2b5e92371ec3b4872820368e19c767c5437264
dmoore9131 <dominique.moore@ymail.com>
2023-08-23 19:59:23 -0700

    task1

--- my_shell.c
+++ my_shell.c
@@ -5,56 +5,53 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-#define MAX_COMMAND_LENGTH 100
-
-void display_prompt() {
-    printf("($) ");
-}
+#define MAX_INPUT_SIZE 1024
 
 int main() {
-    char command[MAX_COMMAND_LENGTH];
-    pid_t pid;
+    char input[MAX_INPUT_SIZE];
+    char *args[2]; // We will only handle commands without arguments
 
     while (1) {
-        display_prompt();
+        // Display the prompt
+        printf("#cisfun$ ");
 
-        if (fgets(command, sizeof(command), stdin) == NULL) {
-            /* Handle end of file (Ctrl+D) */
+        // Read user input
+        if (fgets(input, MAX_INPUT_SIZE, stdin) == NULL) {
+            // Handle end of file (Ctrl+D)
             printf("\n");
             break;
         }
 
-        /* Remove the newline character from the end of the command */
-        command[strcspn(command, "\n")] = '\0';
+        // Remove the newline character
+        input[strcspn(input, "\n")] = '\0';
 
-        /* Check if the user wants to exit */
-        if (strcmp(command, "exit") == 0) {
+        // Check if user wants to exit
+        if (strcmp(input, "exit") == 0) {
             break;
         }
 
-        /* Fork a child process */
-        pid = fork();
+        // Fork a child process
+        pid_t child_pid = fork();
 
-        if (pid == -1) {
+        if (child_pid == -1) {
             perror("fork");
-        }
-        else if (pid == 0) {
-            /* Child process */
-            /* Create an array to hold the command and arguments */
-            char *args[2];
-            args[0] = command;
+        } else if (child_pid == 0) {
+            // Child process
+
+            // Prepare the arguments for execve
+            args[0] = input;
             args[1] = NULL;
 
-            /* Execute the command using execvp */
-            if (execvp(command, args) == -1) {
-                perror("execvp");
+            // Execute the command
+            if (execve(args[0], args, NULL) == -1) {
+                perror("execve");
                 exit(EXIT_FAILURE);
             }
-        }
-        else {
-            /* Parent process */
-            int status;
-            waitpid(pid, &status, 0);
+        } else {
+            // Parent process
+
+            // Wait for the child to complete
+            waitpid(child_pid, NULL, 0);
         }
     }
 
