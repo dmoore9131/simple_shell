0890d5b198e8c7c77c5d94c11ba2c9f5dcebbd2f
dmoore9131 <dominique.moore@ymail.com>
2023-08-25 16:55:25 -0700

    Simple Shell

--- CommandLineParser.c
+++ CommandLineParser.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-int main(int ac, char **av)  
-{       
+int main(int ac, char **av)
+{
     int i;
 
     for (i = ac - 1; i >= 0; i--) {
-        printf("%s\n", av[i]);
+        printf("%d. %s\n", ac - i, av[i]);
     }
-
-    return 0; 
+    printf("%ld", getline());
+    return 0;
 }
 
