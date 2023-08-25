3c9352de5548133796d89f2cd59b894f64180506
dmoore9131 <dominique.moore@ymail.com>
2023-08-25 12:33:57 -0700

    task1

--- simple_shell.c
+++ simple_shell.c
@@ -8,50 +8,36 @@
 #define BUFFER_SIZE 1024
 
 int main(void) {
-    char buffer[BUFFER_SIZE];
-    char *args[BUFFER_SIZE / 2]; // Array to store command and arguments
+    char input[BUFFER_SIZE];
     int should_run = 1;
 
     while (should_run) {
-        printf("$ ");
+        printf("#cisfun$ "); /* Display prompt */
         fflush(stdout);
 
         // Read a line of input from the user
-        if (fgets(buffer, BUFFER_SIZE, stdin) == NULL) {
+        if (fgets(input, BUFFER_SIZE, stdin) == NULL) {
             break;
         }
 
         // Remove the trailing newline character
-        buffer[strcspn(buffer, "\n")] = '\0';
+        input[strcspn(input, "\n")] = '\0';
 
-        // Tokenize the input into command and arguments
-        char *token = strtok(buffer, " ");
-        int arg_count = 0;
-        while (token != NULL) {
-            args[arg_count] = token;
-            arg_count++;
-            token = strtok(NULL, " ");
-        }
-        args[arg_count] = NULL; // Null-terminate the argument list
+        // Fork a child process
+        pid_t pid = fork();
 
-        if (arg_count > 0) {
-            // Fork a child process
-            pid_t pid = fork();
-
-            if (pid < 0) {
-                perror("Fork failed");
+        if (pid < 0) {
+            perror("Fork failed");
+            exit(EXIT_FAILURE);
+        } else if (pid == 0) {
+            // This code runs in the child process
+            if (execlp(input, input, NULL) == -1) {
+                perror("Execution failed");
                 exit(EXIT_FAILURE);
-            } else if (pid == 0) {
-                // This code runs in the child process
-                if (execvp(args[0], args) == -1) {
-                    perror("Execution failed");
-                    exit(EXIT_FAILURE);
-                }
-            } else {
-                // This code runs in the parent process
-                int status;
-                waitpid(pid, &status, 0); // Wait for the child to finish
             }
+        } else {
+            // This code runs in the parent process
+            wait(NULL); // Wait for the child process to finish
         }
     }
 
