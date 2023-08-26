1721703636fe79f9e548ebc6f002689c853addde
dmoore9131 <dominique.moore@ymail.com>
2023-08-25 19:15:31 -0700

    task1

--- CommandLineParser2.c
+++ CommandLineParser2.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-    printf("Number of command-line arguments: %d\n", argc);
-
-    // Print each command-line argument
-    for (int i = 0; i < argc; i++) {
-        printf("Argument %d: %s\n", i, argv[i]);
+    int i;  // Declare 'i' outside of the loop
+    for (i = 0; i < argc; i++) {
+        // Your loop code here
     }
-
     return 0;
 }
 
