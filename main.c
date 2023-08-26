8fabfa783deb4e83398827d61c2ba002eb8a4b2a
dmoore9131 <dominique.moore@ymail.com>
2023-08-25 18:48:59 -0700

    Simple Shell

--- main.c
+++ main.c
@@ -1,7 +1,19 @@
-#include <stdio.h>
+#include "shell.h"
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+int main(void)
+{
+    char *input;
+    char **args;
+
+    while (1)
+    {
+        input = get_input(); // Implement this function to get user input
+        args = split_input(input); // Implement this function to split input into arguments
+        execute(args); // Implement this function to execute commands
+        free(input);
+        free(args);
+    }
+
+    return (0);
 }
 
