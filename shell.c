49012742ae82e652c267eefbfb8a18fc94ca3a93
dmoore9131 <dominique.moore@ymail.com>
2023-08-25 18:28:52 -0700

    task1

--- shell.c
+++ shell.c
@@ -1,16 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h> // Include string.h for strlen
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
-#define BUFFER_SIZE 1024
+#define MAX_INPUT_LENGTH 100
 
 int main() {
-    char command[BUFFER_SIZE];
+    char input[MAX_INPUT_LENGTH];
 
-    printf("Enter a command: ");
-    if (fgets(command, sizeof(command), stdin) != NULL) {
-        command[strlen(command) - 1] = '\0'; // Remove newline character
-        printf("You entered: %s\n", command);
+    while (1) {
+        printf("#cisfun$ "); // Display prompt
+
+        if (fgets(input, MAX_INPUT_LENGTH, stdin) == NULL) {
+            perror("fgets");
+            exit(EXIT_FAILURE);
+        }
+
+        // Remove the trailing newline character if present
+        input[strcspn(input, "\n")] = '\0';
+
+        if (strcmp(input, "exit") == 0) {
+            printf("Exiting shell.\n");
+            break; // Exit the loop
+        }
+
+        pid_t child_pid = fork();
+
+        if (child_pid == -1) {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        }
+
+        if (child_pid == 0) {
+            // This code is executed by the child process
+            // Replace the child process with the desired command
+            execlp(input, input, NULL);
+            // If execlp returns, it means an error occurred
+            perror("execlp");
+            exit(EXIT_FAILURE);
+        } else {
+            // This code is executed by the parent process
+            int status;
+            wait(&status); // Wait for the child process to finish
+
+            if (WIFEXITED(status)) {
+                printf("Child process exited with status %d\n", WEXITSTATUS(status));
+            } else {
+                printf("Child process did not exit normally\n");
+            }
+        }
     }
 
     return 0;
