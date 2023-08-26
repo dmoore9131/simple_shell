58cb62635a85dc53942b745e4f5983c283e89880
dmoore9131 <dominique.moore@ymail.com>
2023-08-25 19:04:59 -0700

    task1

--- simple_shell.c
+++ simple_shell.c
@@ -1,60 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 
-#define MAX_INPUT_LENGTH 100
+#define MAX_COMMAND_LENGTH 100
 
 // Function to execute a command
-void execute_command(char *command) {
-    pid_t pid, wpid;
-    int status;
-
-    pid = fork();
-
-    if (pid == -1) {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    }
-
-    if (pid == 0) {
-        // This code is executed by the child process
-        // Replace the child process with the desired command
-        execlp(command, command, NULL);
-
-        // If execlp returns, it means an error occurred
-        perror("execlp");
-        exit(EXIT_FAILURE);
-    } else {
-        // This code is executed by the parent process
-        do {
-            wpid = waitpid(pid, &status, WUNTRACED);
-        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+void executeCommand(char *command) {
+    if (system(command) == -1) {
+        printf("Error: Command execution failed.\n");
     }
 }
 
 int main() {
-    char input[MAX_INPUT_LENGTH];
+    char input[MAX_COMMAND_LENGTH];
 
     while (1) {
-        printf("#cisfun$ "); // Display prompt
-
-        if (fgets(input, MAX_INPUT_LENGTH, stdin) == NULL) {
-            perror("fgets");
-            exit(EXIT_FAILURE);
+        printf("#cisfun$ "); // Display the shell prompt
+        if (fgets(input, sizeof(input), stdin) == NULL) {
+            printf("\n"); // Handle Ctrl+D (end of file)
+            break;
         }
 
-        // Remove the trailing newline character if present
+        // Remove the trailing newline character
         input[strcspn(input, "\n")] = '\0';
 
         if (strcmp(input, "exit") == 0) {
-            printf("Exiting shell.\n");
-            break; // Exit the loop
+            break; // Exit the shell
         }
 
-        execute_command(input);
+        executeCommand(input); // Execute the user's command
     }
 
     return 0;
