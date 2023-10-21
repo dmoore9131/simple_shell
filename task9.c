bdbadd04082cd1991903d93ddd232c75985dfd77
dmoore9131 <dominiquemoore776@gmail.com>
2023-10-21 07:08:47 -0700

    task9

--- task9.c
+++ task9.c
@@ -5,20 +5,16 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-#define MAX_ENV_VAR_LENGTH 128
-#define MAX_ENV_VALUE_LENGTH 256
-#define MAX_BUFFER_SIZE 1024
-#define MAX_ARG_COUNT 32
+#define BUFFER_SIZE 1024
+#define MAX_ARGS 32
 
 int commandExists(const char *cmd);
 void printEnvironment(void);
-void setEnvironmentVariable(const char *var, const char *value);
-void unsetEnvironmentVariable(const char *var);
 
 int main(void) {
     char *buffer = NULL;
     size_t bufsize = 0;
-    char *args[MAX_ARG_COUNT];
+    char *args[MAX_ARGS];
     int status;
 
     while (1) {
@@ -39,7 +35,7 @@ int main(void) {
         char *token = strtok(buffer, " ");
         int i = 0;
 
-        while (token != NULL && i < MAX_ARG_COUNT - 1) {
+        while (token != NULL && i < MAX_ARGS - 1) {
             args[i] = token;
             i++;
             token = strtok(NULL, " ");
@@ -60,14 +56,18 @@ int main(void) {
             } else if (strcmp(args[0], "env") == 0) {
                 printEnvironment();
             } else if (strcmp(args[0], "setenv") == 0) {
-                if (i == 3) {
-                    setEnvironmentVariable(args[1], args[2]);
+                if (args[1] != NULL && args[2] != NULL) {
+                    if (setenv(args[1], args[2], 1) == -1) {
+                        fprintf(stderr, "Failed to set environment variable\n");
+                    }
                 } else {
                     fprintf(stderr, "Usage: setenv VARIABLE VALUE\n");
                 }
             } else if (strcmp(args[0], "unsetenv") == 0) {
-                if (i == 2) {
-                    unsetEnvironmentVariable(args[1]);
+                if (args[1] != NULL) {
+                    if (unsetenv(args[1]) == -1) {
+                        fprintf(stderr, "Failed to unset environment variable\n");
+                    }
                 } else {
                     fprintf(stderr, "Usage: unsetenv VARIABLE\n");
                 }
@@ -100,7 +100,7 @@ int commandExists(const char *cmd) {
     char *path_token = strtok(path_copy, ":");
 
     while (path_token != NULL) {
-        char cmd_path[MAX_BUFFER_SIZE];
+        char cmd_path[1024];
         snprintf(cmd_path, sizeof(cmd_path), "%s/%s", path_token, cmd);
         if (access(cmd_path, X_OK) == 0) {
             free(path_copy);
@@ -117,18 +117,6 @@ void printEnvironment() {
     extern char **environ;
     for (char **env = environ; *env; env++) {
         printf("%s\n", *env);
-    }
-}
-
-void setEnvironmentVariable(const char *var, const char *value) {
-    if (setenv(var, value, 1) == -1) {
-        fprintf(stderr, "Failed to set environment variable\n");
-    }
-}
-
-void unsetEnvironmentVariable(const char *var) {
-    if (unsetenv(var) == -1) {
-        fprintf(stderr, "Failed to unset environment variable\n");
     }
 }
 
