fd427d2b61c674cccf94a8d57bb2275ced74b5bb
dmoore9131 <dominiquemoore776@gmail.com>
2023-10-21 10:37:29 -0700

    task9

--- task9.c
+++ task9.c
@@ -2,129 +2,78 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 
-#define BUFFER_SIZE 1024
-#define MAX_ARGS 32
-
-int commandExists(const char *cmd);
-void printEnvironment(void);
-
-void _setenv(const char *variable, const char *value) {
-    if (setenv(variable, value, 1) == -1) {
-        fprintf(stderr, "Failed to set environment variable\n");
-    }
-}
-
-void _unsetenv(const char *variable) {
-    if (unsetenv(variable) == -1) {
-        fprintf(stderr, "Failed to unset environment variable\n");
-    }
-}
+// Function prototypes for your built-in commands
+void execute_setenv(char *command);
+void execute_unsetenv(char *command);
 
 int main(void) {
-    char *buffer = NULL;
-    size_t bufsize = 0;
-    char *args[MAX_ARGS];
-    int status;
+    char *input;
+    size_t len = 0;
+    ssize_t read;
 
     while (1) {
-        printf(":) ");
-        if (getline(&buffer, &bufsize, stdin) == -1) {
-            if (isatty(STDIN_FILENO)) {
-                printf("\n");
-            }
-            free(buffer);
-            exit(0);
+        printf("#cisfun$ ");
+        read = getline(&input, &len, stdin);
+
+        if (read == -1) {
+            perror("getline");
+            free(input);
+            exit(EXIT_FAILURE);
         }
 
-        size_t len = strlen(buffer);
-        if (len > 0 && buffer[len - 1] == '\n') {
-            buffer[len - 1] = '\0';
+        // Check for built-in commands and execute them
+        if (strncmp(input, "setenv", 6) == 0) {
+            execute_setenv(input);
+        } else if (strncmp(input, "unsetenv", 8) == 0) {
+            execute_unsetenv(input);
+        } else {
+            // Handle other commands or external programs here
         }
 
-        char *token = strtok(buffer, " ");
-        int i = 0;
-
-        while (token != NULL && i < MAX_ARGS - 1) {
-            args[i] = token;
-            i++;
-            token = strtok(NULL, " ");
-        }
-
-        if (i > 0) {
-            args[i] = NULL;
-
-            if (strcmp(args[0], "exit") == 0) {
-                if (args[1] != NULL) {
-                    int exit_status = atoi(args[1]);
-                    free(buffer);
-                    exit(exit_status);
-                } else {
-                    free(buffer);
-                    exit(0);
-                }
-            } else if (strcmp(args[0], "env") == 0) {
-                printEnvironment();
-            } else if (strcmp(args[0], "setenv") == 0) {
-                if (args[1] != NULL && args[2] != NULL) {
-                    _setenv(args[1], args[2]);
-                } else {
-                    fprintf(stderr, "Usage: setenv VARIABLE VALUE\n");
-                }
-            } else if (strcmp(args[0], "unsetenv") == 0) {
-                if (args[1] != NULL) {
-                    _unsetenv(args[1]);
-                } else {
-                    fprintf(stderr, "Usage: unsetenv VARIABLE\n");
-                }
-            } else if (commandExists(args[0])) {
-                pid_t child_pid = fork();
-                if (child_pid == -1) {
-                    perror("fork");
-                    exit(1);
-                }
-                if (child_pid == 0) {
-                    execvp(args[0], args);
-                    perror("execvp");
-                    exit(1);
-                } else {
-                    wait(&status);
-                }
-            } else {
-                printf("Command not found: %s\n", args[0]);
-            }
-        }
+        free(input);
+        input = NULL;
     }
 
-    free(buffer);
     return 0;
 }
 
-int commandExists(const char *cmd) {
-    char *path = getenv("PATH");
-    char *path_copy = strdup(path);
-    char *path_token = strtok(path_copy, ":");
+void execute_setenv(char *command) {
+    // Parse the command and extract the variable name and value
+    char *tokens = strtok(command, " ");
+    char *name = strtok(NULL, " ");
+    char *value = strtok(NULL, " ");
 
-    while (path_token != NULL) {
-        char cmd_path[1024];
-        snprintf(cmd_path, sizeof(cmd_path), "%s/%s", path_token, cmd);
-        if (access(cmd_path, X_OK) == 0) {
-            free(path_copy);
-            return 1;
-        }
-        path_token = strtok(NULL, ":");
+    if (name == NULL) {
+        fprintf(stderr, "Usage: setenv <variable_name> [variable_value]\n");
+        return;
     }
 
-    free(path_copy);
-    return 0;
+    // Set the environment variable
+    if (value != NULL) {
+        if (setenv(name, value, 1) != 0) {
+            perror("setenv");
+        }
+    } else {
+        if (setenv(name, "", 1) != 0) {
+            perror("setenv");
+        }
+    }
 }
 
-void printEnvironment() {
-    extern char **environ;
-    for (char **env = environ; *env; env++) {
-        printf("%s\n", *env);
+void execute_unsetenv(char *command) {
+    // Parse the command and extract the variable name
+    char *tokens = strtok(command, " ");
+    char *name = strtok(NULL, " ");
+
+    if (name == NULL) {
+        fprintf(stderr, "Usage: unsetenv <variable_name>\n");
+        return;
+    }
+
+    // Unset the environment variable
+    if (unsetenv(name) != 0) {
+        perror("unsetenv");
     }
 }
 
